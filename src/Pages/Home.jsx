4469a66b99659a7b1dc705eb3946a88fb5269c2f
Jose Rodriguez <jose.rodriguez1901@alumnos.ubiobio.cl>
2023-10-17 01:02:20 -0300

    Arreglo de errores

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,8 +17,13 @@ function Home() {
   const [candidato, setCandidato] = useState('');
   const [aceptados, setAceptados] = useState([]);
   const [rechazados, setRechazados] = useState([]);
-  const [expanded, setExpanded] = React.useState(false);
+  const [expandedA, setExpandedA] = React.useState(false);
+  const [expandedR, setExpandedR] = React.useState(false);
   const [botonesDeshabilitados, setBotonesDeshabilitados] = useState(false);
+  const numeroRamdomNombre = Math.floor(Math.random() * nombresPerros.length);
+  const numeroRamdomDescripcion = Math.floor(Math.random() * descripcionesPerros.length);
+  const [openItem, setOpenItem] = useState(null);
+  const [openItemR, setOpenItemR] = useState(null);
 
   useEffect(() => {
     obtenerNuevaFoto();
@@ -38,8 +43,8 @@ function Home() {
 
   const aceptarPerro = () => {
     // Crea un nuevo perro aceptado con datos aleatorios y la foto actual del candidato
-    const nombreAleatorio = nombresPerros[Math.floor(Math.random() * nombresPerros.length)];
-    const descripcionAleatoria = descripcionesPerros[Math.floor(Math.random() * descripcionesPerros.length)];
+    const nombreAleatorio = nombresPerros[numeroRamdomNombre];
+    const descripcionAleatoria = descripcionesPerros[numeroRamdomDescripcion];
     const nuevoPerroAceptado = {
       url: candidato,
       nombre: nombreAleatorio,
@@ -56,8 +61,8 @@ function Home() {
   const rechazarPerro = () => {
 
     // Crea un nuevo perro rechazado con datos aleatorios y la foto actual del candidato
-    const nombreAleatorio = nombresPerros[Math.floor(Math.random() * nombresPerros.length)];
-    const descripcionAleatoria = descripcionesPerros[Math.floor(Math.random() * descripcionesPerros.length)];
+    const nombreAleatorio = nombresPerros[numeroRamdomNombre];
+    const descripcionAleatoria = descripcionesPerros[numeroRamdomDescripcion];
     const nuevoPerroRechazado = {
       url: candidato,
       nombre: nombreAleatorio,
@@ -71,7 +76,7 @@ function Home() {
     obtenerNuevaFoto();
   };
 
-  const ExpandMore = styled((props) => {
+  const ExpandMoreA = styled((props) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
   })(({ theme, expand }) => ({
@@ -82,11 +87,42 @@ function Home() {
     }),
   }));
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const handleExpandClickA = (itemIndex) => {
+    console.log(itemIndex);
+    if (itemIndex === openItem) {
+      // Si se hace clic en el elemento ya abierto, ciérralo.
+      setOpenItem(null);
+      //setExpandedA(null);
+      setExpandedA(!expandedA);
+    } else {
+      setOpenItem(itemIndex);
+    }
   };
 
-  const renderPerros = (perros, titulo) => (
+  const ExpandMoreR = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  const handleExpandClickR = (itemIndex) => {
+    if (itemIndex === openItemR) {
+      // Si se hace clic en el elemento ya abierto, ciérralo.
+      setOpenItemR(null);
+      //setExpandedA(null);
+      setExpandedR(!expandedR);
+    } else {
+      setOpenItemR(itemIndex);
+    }
+  };
+  
+
+  const renderPerrosA = (perros, titulo) => (
     <Grid item xs={12} sm={4}>
       <Typography variant="h6">{titulo}</Typography>
       {perros.map((perro, index) => (
@@ -98,22 +134,62 @@ function Home() {
                 {perro.nombre}
               </Typography>
               <CardActions disableSpacing>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
+                <ExpandMoreA
+                  expand={expandedA}
+                  onClick={()=>handleExpandClickA(index)}
+                  aria-expanded={expandedA}
                   aria-label="show more"
                 >
                   <ExpandMoreIcon />
-                </ExpandMore>
+                </ExpandMoreA>
               </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
+              {openItem === index && (
+              <Collapse in={expandedA} timeout="auto"  >
                 <CardContent>
                   <Typography variant="body2" color="text.secondary">
                     {perro.descripcion}
                   </Typography>
                 </CardContent>
-              </Collapse>
+              </Collapse>)}
+            </CardContent>
+            <CardActions>
+              <Button size="small">Quitar</Button>
+            </CardActions>
+          </Card>
+        )
+      ))}
+    </Grid>
+  );
+
+  const renderPerrosR = (perros, titulo) => (
+    <Grid item xs={12} sm={4}>
+      <Typography variant="h6">{titulo}</Typography>
+      {perros.map((perro, index) => (
+        perro.url && (
+          <Card key={index} style={cardStyle}>
+            <img src={perro.url} alt={`${titulo.toLowerCase()} ${index}`} style={{ height: '140px', objectFit: 'cover' }} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {perro.nombre}
+              </Typography>
+              <CardActions disableSpacing>
+                <ExpandMoreR
+                  expand={expandedR}
+                  onClick={()=>handleExpandClickR(index)}
+                  aria-expanded={expandedR}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </ExpandMoreR>
+              </CardActions>
+              {openItemR === index && (
+                <Collapse in={expandedR} timeout="auto" unmountOnExit >
+                  <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                      {perro.descripcion}
+                    </Typography>
+                  </CardContent>
+                </Collapse>)}
             </CardContent>
             <CardActions>
               <Button size="small">Quitar</Button>
@@ -130,8 +206,9 @@ function Home() {
         <Card style={cardStyle}>
           <img src={candidato} alt="Candidato" style={{ height: '140px', objectFit: 'cover' }} />
           <CardContent>
-            <Typography variant="h6">{nombresPerros[Math.floor(Math.random() * nombresPerros.length)]}</Typography>
-            <CardActions disableSpacing>
+            <Typography variant="h6">{nombresPerros[numeroRamdomNombre]}</Typography>
+            
+            {/* <CardActions disableSpacing>
               <ExpandMore
                 expand={expanded}
                 onClick={handleExpandClick}
@@ -140,12 +217,12 @@ function Home() {
               >
                 <ExpandMoreIcon />
               </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography variant="body2">{descripcionesPerros[Math.floor(Math.random() * descripcionesPerros.length)]}</Typography>
-              </CardContent>
-            </Collapse>
+            </CardActions> */}
+            {/* <Collapse in={expanded} timeout="auto" unmountOnExit> */}
+              {/* <CardContent> */}
+                <Typography variant="body2">{descripcionesPerros[numeroRamdomDescripcion]}</Typography>
+              {/* </CardContent> */}
+            {/* </Collapse> */}
           </CardContent>
           <CardActions>
             <Button variant="contained" color="primary" onClick={aceptarPerro} disabled={botonesDeshabilitados}>
@@ -157,8 +234,8 @@ function Home() {
           </CardActions>
         </Card>
       </Grid>
-      {renderPerros(aceptados, 'Perros Aceptados')}
-      {renderPerros(rechazados, 'Perros Rechazados')}
+      {renderPerrosA(aceptados, 'Perros Aceptados')}
+      {renderPerrosR(rechazados, 'Perros Rechazados')}
     </Grid>
   );
 }
